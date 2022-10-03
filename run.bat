e7402631e291c638744bc1dce00eb899e5609eeb
Victor2297 <59939728+Victor2297@users.noreply.github.com>
2022-10-04 00:12:03 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-call C:/Users/Victor/PycharmProjects/web_banking/venv/Scripts/activate.bat
-pytest --browser=chrome C:\Users\Victor\PycharmProjects\web_banking\tests\step_defs\test_TC_003_new_account.py
+call ../web_banking/venv/Scripts/activate.bat
+pytest --browser=chrome ../web_banking\tests\step_defs\test_TC_003_new_account.py
